a982f36559edc11ff251b659733e9f20e244f9b9
LPAde <andre-peters01@hotmail.de>
2022-04-12 17:55:49 +0200

    MOvement mit Maus ALpha

--- ProjectAtlantis/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ ProjectAtlantis/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,19 +1,21 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace PlayerScripts
 {
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private Player player;
-        
-        [Header("Movement")]
-        [SerializeField] private Vector3 movementVector;
+
+        [Header("Movement")] 
+        [SerializeField] private NavMeshAgent navMeshAgent;
+        [SerializeField] private Vector3 movePos;
         [SerializeField] private float speed;
 
         private void Start()
         {
-            movementVector = transform.position;
+            movePos = transform.position;
         }
 
         private void Update()
@@ -34,7 +36,15 @@ namespace PlayerScripts
                 projectile.Initialize(GameManager.Instance.RhythmManager.CheckTiming(), Vector3.forward);
             }
 
-            movementVector += new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));
+            if (Input.GetMouseButton(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit)) {
+                    movePos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                }
+            }
         }
 
         /// <summary>
@@ -42,10 +52,7 @@ namespace PlayerScripts
         /// </summary>
         private void Move()
         {
-            var position = transform.position;
-            position = Vector3.Lerp(position, movementVector, speed * Time.deltaTime);
-            transform.position = position;
-            movementVector = position;
+            navMeshAgent.SetDestination(movePos);
         }
     }
 }
