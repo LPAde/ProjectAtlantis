4f05ede06ca9ea541758137963563a64789a9429
LPAde <andre-peters01@hotmail.de>
2022-08-12 02:23:31 +0200

    Filled Credits up with our information

--- ProjectAtlantis/Assets/Scripts/UI/CreditsBehaviour.cs
+++ ProjectAtlantis/Assets/Scripts/UI/CreditsBehaviour.cs
@@ -6,15 +6,13 @@ namespace UI
     public class CreditsBehaviour : MonoBehaviour
     {
         [SerializeField] private float scrollSpeed;
-        [SerializeField] private float duration;
+        [SerializeField] private float finalHeight;
         
         void Update()
         {
             transform.position += new Vector3(0,Time.deltaTime * scrollSpeed,0);
-
-            duration -= Time.deltaTime;
-
-            if (duration < 0 || Input.GetKeyDown(KeyCode.Escape))
+            
+            if (transform.position.y > finalHeight || Input.GetKeyDown(KeyCode.Escape))
                 SceneManager.LoadScene(0);
         }
     }
