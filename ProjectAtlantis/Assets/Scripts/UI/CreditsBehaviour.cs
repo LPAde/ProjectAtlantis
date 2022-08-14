e3829b95cd852c5ac360423f9ef646203ac58560
LPAde <andre-peters01@hotmail.de>
2022-08-14 22:10:44 +0200

    Fixed Credits Bug

--- ProjectAtlantis/Assets/Scripts/UI/CreditsBehaviour.cs
+++ ProjectAtlantis/Assets/Scripts/UI/CreditsBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,13 +8,24 @@ namespace UI
     {
         [SerializeField] private float scrollSpeed;
         [SerializeField] private float finalHeight;
-        
+        private Resolution _res;
+
+        private void Start()
+        {
+            // Prevent the resolution hurting the credits.
+            _res = Screen.currentResolution;
+            Screen.SetResolution(1280, 720, FullScreenMode.FullScreenWindow);
+        }
+
         void Update()
         {
             transform.position += new Vector3(0,Time.deltaTime * scrollSpeed,0);
+
+            if (!(transform.position.y > finalHeight) && !Input.GetKeyDown(KeyCode.Escape)) 
+                return;
             
-            if (transform.position.y > finalHeight || Input.GetKeyDown(KeyCode.Escape))
-                SceneManager.LoadScene(0);
+            Screen.SetResolution(_res.width,_res.height,FullScreenMode.FullScreenWindow);
+            SceneManager.LoadScene(0);
         }
     }
 }
